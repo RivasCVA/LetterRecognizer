20a6fd5dcf0aa4052c9bd99afa6ea41ffcd7adba
Carlos Rivas <rivascarlosva@gmail.com>
2020-02-05 11:54:50 -0600

    added starting tensorflow file

---
+++ LREngine.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class LREngine:
+    def __init__(self):
+        print("initialized")
+
+engine = LREngine()
