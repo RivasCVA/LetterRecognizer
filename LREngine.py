f3a7bd9d5d7af2d7133ba283924820254c9bb451
Carlos Rivas <rivascarlosva@gmail.com>
2020-02-10 12:05:15 -0600

    ASCII dictionary to convert CNN output

--- LREngine.py
+++ LREngine.py
@@ -8,7 +8,19 @@ import os
 import MNIST_format_extractor as gze
 
 class LREngine:
+    # Model holds the trained CNN
     model = None
+    
+    # Dictionary converts the label id to the ASCII id
+    class_names = {
+            0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 
+            10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H', 18: 'I', 
+            19: 'J', 20: 'K', 21: 'L', 22: 'M', 23: 'N', 24: 'O', 25: 'P', 26: 'Q', 27: 'R', 
+            28: 'S', 29: 'T', 30: 'U', 31: 'V', 32: 'W', 33: 'X', 34: 'Y', 35: 'Z', 36: 'a', 
+            37: 'b', 38: 'c', 39: 'd', 40: 'e', 41: 'f', 42: 'g', 43: 'h', 44: 'i', 45: 'j', 
+            46: 'k', 47: 'l', 48: 'm', 49: 'n', 50: 'o', 51: 'p', 52: 'q', 53: 'r', 54: 's', 
+            55: 't', 56: 'u', 57: 'v', 58: 'w', 59: 'x', 60: 'y', 61: 'z' }
+
 
     def train_model():
         # Will check to see if trained model already exists
@@ -27,9 +39,6 @@ class LREngine:
         with tf.io.gfile.GFile(os.path.expanduser(
             "~/.keras/datasets/EMNIST/emnist-byclass-train-labels-idx1-ubyte.gz"), "rb") as f:
             train_labels = gze.extract_labels(f)
-
-        # Dictionary converts the label id to the ASCII id
-        class_names = []
 
         # Display the details of the train data
         print("Train Images (amount, width, height): ", end = '')
@@ -76,7 +85,7 @@ class LREngine:
         # Gets and prints the predictions from the model
         prediction_indexes = LREngine.getPredictions(test_images)
         for i in range(len(prediction_indexes)):
-            print("Prediction: " + str(prediction_indexes[i]) + "\t\tActual: " + str(test_labels[i]))
+            print("Prediction: " + str(LREngine.class_names[prediction_indexes[i]]) + "\t\tActual: " + str(LREngine.class_names[test_labels[i]]))
 
     def showImages(images, labels, n=1):
         # Shows the first n images along with their labels
